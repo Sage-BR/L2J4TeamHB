f2f3ddb1312dc6a485613f573dc96b5e9ccff1a2
Saulo Germano <sgwebr@gmail.com>
2026-07-20 09:59:26 -0300

    fix bats

--- data/Server/startGameServer.bat
+++ data/Server/startGameServer.bat
@@ -4,11 +4,11 @@ title L2J4Team GameServer Console
 echo Starting L2J4Team Game Server.
 echo.
 REM -------------------------------------
-REM Default parameters for a basic server.
-java -Xmx1g -server -XX:+UseG1GC -XX:+TieredCompilation -XX:+UseCompressedOops -XX:SurvivorRatio=8 -XX:NewRatio=4 -cp "%~dp0lib\*;%~dp0l2jserver.jar" net.sf.l2j.gameserver.GameServer
+REM Default parameters for a basic server (Java 25 / G1GC).
+java -Xms512m -Xmx1g -server -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+ParallelRefProcEnabled -XX:+UseStringDeduplication -cp "%~dp0lib\*;%~dp0l2jserver.jar" net.sf.l2j.gameserver.GameServer
 REM
 REM If you have a big server and lots of memory, you could experiment for example with
-REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
+REM java -server -Xms1536m -Xmx1536m -XX:+UseG1GC -XX:MaxGCPauseMillis=150 -XX:G1HeapRegionSize=8m -XX:+ParallelRefProcEnabled -XX:+UseStringDeduplication -XX:+AlwaysPreTouch -cp "%~dp0lib\*;%~dp0l2jserver.jar" net.sf.l2j.gameserver.GameServer
 REM -------------------------------------
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
